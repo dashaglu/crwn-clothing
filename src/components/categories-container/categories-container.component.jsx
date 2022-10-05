bc5874af6dc7b3ca033ff712fcc694dda542bcb3
DashaGlu <darya@gluschenko.by>
2022-10-05 22:16:39 +0200

    Create CategoriesContainer and CategoryItems components and add styles for them.

---
+++ src/components/categories-container/categories-container.component.jsx
@@ -0,0 +1,11 @@
+import './categories-container.styles.scss';
+
+import CategoryItem from "../category-item/category-item.component";
+
+const CategoriesContainer = ({ categories }) => (
+    <div className="categories-container">
+        {categories.map((category) => <CategoryItem category={category} key={category.id}/>)}
+    </div>
+);
+
+export default CategoriesContainer;
