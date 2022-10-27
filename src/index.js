cd81b16b3c7e2c41bd41c8b3b00344305c2c12f6
DashaGlu <darya@gluschenko.by>
2022-10-27 17:54:05 +0200

    Add payment ability with Stripe service.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from '@stripe/react-stripe-js';
 
 import { store, persistor } from './store/store';
+import { stripePromise } from './utils/stripe/stripe.utils';
 
 import App from './App';
 
@@ -17,7 +19,9 @@ root.render(
         <Provider store={store}>
             <PersistGate persistor={persistor}>
                 <BrowserRouter>
-                    <App />
+                    <Elements stripe={stripePromise}>
+                        <App />
+                    </Elements>
                 </BrowserRouter>
             </PersistGate>
         </Provider>
